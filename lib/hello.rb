89507859e0464e2d9201535494c6954288b0dee9
Joy Lin <vvlnote@hotmail.com>
2019-02-15 23:54:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,11 @@ def hello_t(array)
   i = 0
   
   while i < array.length
-    yield array[i]
+    yield array[i] {
+      |array[i] | if array[i].start_with?{"T"}
+                  puts "Hi, #{array[i]}"
+                end
+    }
     i = i + 1
   end
   array
